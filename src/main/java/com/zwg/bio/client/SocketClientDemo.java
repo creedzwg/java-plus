7c0302b4d8380808d0c75aa4512b82031394888c
zhangwengang <zhangwengang>
2020-04-19 01:04:31 +0800

    commit

--- src/main/java/com/zwg/bio/client/SocketClientDemo.java
+++ src/main/java/com/zwg/bio/client/SocketClientDemo.java
@@ -14,6 +14,7 @@ public class SocketClientDemo {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         Socket socket = new Socket("127.0.0.1", 6778);
+
         OutputStream outputStream = socket.getOutputStream();
         byte[] bytes = "wert34343xzxzq切尔奇二二翁2亲亲我群无12122121".getBytes("UTF-8");
         System.out.println(bytes.length);
